c51b9d1fb946b7b5e7eddb6406359df2bb9efe23
nishanthr878 <nishanthr878@gmail.com>
2026-06-03 22:12:35 +0530

    first commit

---
+++ src/main/java/com/llmscoring/LlmScoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.llmscoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmScoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmScoringServiceApplication.class, args);
+	}
+
+}
